935441ec426b95d86170674a56597f0da1721b06
ism4jly <ismaelpaulofreitas@gmail.com>
2025-05-27 12:42:30 -0300

    test: testar a classe Tarefa

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,12 @@
 public class Main {
     public static void main(String[] args) {
 
+        Tarefa tarefa1 = new Tarefa("estudar java", false);
+
+        System.out.println("Descrição: " + tarefa1.getDescricao());
+        tarefa1.marcarConcluida();
+        System.out.println("Concluida? " + tarefa1.isConcluida());
+        System.out.println("toString: " + tarefa1);
+
+    }
 }
